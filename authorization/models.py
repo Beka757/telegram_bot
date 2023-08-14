aa8c30ad73f683086134c05428cd83bf921ce037
beka <beka1990.30.10@gmail.com>
2023-08-14 14:20:40 +0600

    fixed user model

--- authorization/models.py
+++ authorization/models.py
@@ -8,7 +8,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, verbose_name='Логин', unique=True)
     name = models.CharField(max_length=255, verbose_name='Имя')
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
     USERNAME_FIELD = 'username'
